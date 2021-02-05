647f7165148fb95e5f082b4182f338afeecbd4d5
ruby <dhdmsdud97@gmail.com>
2021-02-05 13:44:45 +0900

    maple

---
+++ WEB/CAFE_MAPLE/maple/mapleApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+#----------------------< 공통 >----------------------
+admin.site.register(SampleProduct)
+
+#----------------------< 오은영 >----------------------
+admin.site.register(Staff_info)
